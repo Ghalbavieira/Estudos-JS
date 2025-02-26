5f87388d9454a8cafb6d2e6709f2ad9c4204cbba
Ghalba Vieira <72039896+Ghalbavieira@users.noreply.github.com>
2025-02-26 00:26:01 +0000

    feat:  training function in js

---
+++ desafio-extra/dia02.js
@@ -0,0 +1,11 @@
+
+
+function conversorTemperatura(){
+    let celsius = parseFloat( prompt("Temperatura em graus Celsius: ") );
+    let fahrenheit =  celsius * 9/5 + 32
+    console.log("a temperatura em fahrenheit é " + fahrenheit);
+}
+
+conversorTemperatura()
+
+
---
+++ desafios-31dias/function-dia07.js
@@ -0,0 +1,23 @@
+/**
+ *  1. Crie uma função que calcule o dobro de um número.
+    2. Escreva uma função que receba um nome e retorne uma mensagem de boas-vindas.
+    3. Crie uma função que receba dois números e retorne o maior deles.
+    4. Implemente uma função que calcule a média de três números.
+    5. Crie uma função que receba um array de números e retorne a soma deles.
+    6. Escreva uma função que receba uma string e retorne o número de caracteres dessa string.
+ * 
+ */
+
+function calculeDobroNumeros(number1){
+    return number1 * 2
+
+}
+console.log(calculeDobroNumeros(6))
+
+
+function receiveName(nome){
+    return `Olá ${nome} seja bem vindo!`
+}
+
+console.log(receiveName('Carlos'))
+
